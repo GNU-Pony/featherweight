e81851cf01c81594c0191a7e8894bfed28306a88
Mattias Andrée <maandree@operamail.com>
2015-12-03 17:45:44 +0100

    bump year
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,7 @@
 '''
 featherweight – A lightweight terminal news feed reader
 
-Copyright © 2013, 2014  Mattias Andrée (maandree@member.fsf.org)
+Copyright © 2013, 2014, 2015  Mattias Andrée (maandree@member.fsf.org)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
