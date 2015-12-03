96a1694c5b32d24e22b64a9f7b69d6a8f380cb37
Mattias Andrée <maandree@operamail.com>
2015-12-03 17:57:02 +0100

    doc common.py + do not require that HOME is set
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/common.py
+++ src/common.py
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import os
+import pwd
 import gettext
 
 gettext.bindtextdomain('@PKGNAME@', '@LOCALEDIR@')
@@ -24,14 +25,54 @@ gettext.textdomain('@PKGNAME@')
 _ = gettext.gettext
 
 
+
 islinux = ('TERM' in os.environ) and (os.environ['TERM'] == 'linux')
-home = os.environ['HOME']
+'''
+:bool  Running in Linux VT?  This is useful for selecting the best characters,
+       as Linux VT has a limited character set.
+'''
+
+home = os.environ['HOME'] if 'HOME' in os.environ else None
+'''
+:str  The user's home directrory.
+'''
+if (home is None) or (len(home) == 0):
+    home = pwd.getpwuid(os.getuid()).pw_dir
 
 quote = (lambda x : _("'%s'") % x) if islinux else (lambda x : _('‘%s’') % x)
+'''
+Enclose a string in single-quotes
+
+@param   x:str  The string
+@return  :str   The string enclosed in quotes
+'''
+
 double_quote = (lambda x : _('"%s"') % x) if islinux else (lambda x : _('“%s”') % x)
+'''
+Enclose a string in double-quotes
+
+@param   x:str  The string
+@return  :str   The string enclosed in quotes
+'''
+
 abbr = lambda x : ('~%s' % x[len(home):] if x.startswith(home + '/') else x)
+'''
+Abbreviate a path
+
+The replaces an the home directory with '~'
+
+@param   x:str  The path
+@return  :str   The path abbreviated
+
+'''
 
 root = '%s/.var/lib/%s' % (home, 'featherweight')
+'''
+:str  Where the program stores all data files
+'''
 
 terminated = False
+'''
+:bool  Exiting?
+'''
 
